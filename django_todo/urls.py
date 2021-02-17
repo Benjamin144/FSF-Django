54fee3817f0c2fa1223370ddd44c8b1b5ef7367e
Benjamin144 <josephroberts@email.com>
2021-02-17 01:31:16 +0000

    refactor test code

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from todo import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.get_todo_list, name='get_todo_list'),
--- todo/views.py
+++ todo/views.py
@@ -11,12 +11,12 @@ def get_todo_list(request):
     return render(request, 'todo/todo_list.html', context)
 
 
-def add_item(request):
+def add_item(request): 
     if request.method == 'POST':
         form = ItemForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('get_todo_list')
+            return redirect('get_todo_list')
     form = ItemForm()
     context = {
         'form': form
